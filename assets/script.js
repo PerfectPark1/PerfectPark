74f265befe1a2611a4556dbb32211201bac44a11
Kevin Holder <kholder777@yahoo.com>
2020-05-28 19:47:05 -0500

    Search button works! This closes #41

--- assets/script.js
+++ assets/script.js
@@ -2,138 +2,124 @@
 var select = document.getElementById("selectState");
 
 var stateCode = [
-  "AL",
-  "AK",
-  "AS",
-  "AZ",
-  "AR",
-  "CA",
-  "CO",
-  "CT",
-  "DE",
-  "DC",
-  "FM",
-  "FL",
-  "GA",
-  "GU",
-  "HI",
-  "ID",
-  "IL",
-  "IN",
-  "IA",
-  "KS",
-  "KY",
-  "LA",
-  "ME",
-  "MH",
-  "MD",
-  "MA",
-  "MI",
-  "MN",
-  "MS",
-  "MO",
-  "MT",
-  "NE",
-  "NV",
-  "NH",
-  "NJ",
-  "NM",
-  "NY",
-  "NC",
-  "ND",
-  "MP",
-  "OH",
-  "OK",
-  "OR",
-  "PW",
-  "PA",
-  "PR",
-  "RI",
-  "SC",
-  "SD",
-  "TN",
-  "TX",
-  "UT",
-  "VT",
-  "VI",
-  "VA",
-  "WA",
-  "WV",
-  "WI",
-  "WY",
+	"AL",
+	"AK",
+	"AS",
+	"AZ",
+	"AR",
+	"CA",
+	"CO",
+	"CT",
+	"DE",
+	"DC",
+	"FM",
+	"FL",
+	"GA",
+	"GU",
+	"HI",
+	"ID",
+	"IL",
+	"IN",
+	"IA",
+	"KS",
+	"KY",
+	"LA",
+	"ME",
+	"MH",
+	"MD",
+	"MA",
+	"MI",
+	"MN",
+	"MS",
+	"MO",
+	"MT",
+	"NE",
+	"NV",
+	"NH",
+	"NJ",
+	"NM",
+	"NY",
+	"NC",
+	"ND",
+	"MP",
+	"OH",
+	"OK",
+	"OR",
+	"PW",
+	"PA",
+	"PR",
+	"RI",
+	"SC",
+	"SD",
+	"TN",
+	"TX",
+	"UT",
+	"VT",
+	"VI",
+	"VA",
+	"WA",
+	"WV",
+	"WI",
+	"WY",
 ];
 
 for (var i = 0; i < stateCode.length; i++) {
-  var opt = stateCode[i];
-  var el = document.createElement("option");
-  el.textContent = opt;
-  el.value = opt; // stateCode instead of opt?
-  select.appendChild(el); // select is defined above on 2 and also here?
+	var opt = stateCode[i];
+	var el = document.createElement("option");
+	el.setAttribute("value", opt);
+	el.textContent = opt;
+	select.appendChild(el);
 }
-// ^^^ i think we need to assign a option value so we can pull the selected item by that value....just a thought.
-// ^
-
-// Why do we have 77-83? it doesnt really do anything....? at this time...maybe after we get the stateCode value...
-// call for the search button to activate the function to search for the state and city info
-// $(document).on("click", "#search", function () {
-//   let NPSqueryURL = $(".form-control").val();
-//   stateCode = $("#selectState").val();
-//   console.log(stateCode);
-//   console.log(NPSqueryURL);
-// });
 
 $(document).ready(function () {
-  $("submitBtn").on("click", function () {
-    stateCode[i] = $("#selectState").find("option:selected").val();
-    stateCode[i] = $(this).val();
-  });
+	$("#submitBtn").on("click", function () {
+		let stateCode = $("#selectState option:selected").text();
+		console.log(stateCode);
+	});
 });
-// $("select").val("option-value");
-// stateCode = $(this).children("option:value").val();
-// stateCode = $("#selectState option:selected").text();
-// stateCode = $("#selectState").val("option");
+$("select").val("option-value");
 
 // Ajax for State
 const NPSAPIkey = "UOZg2ZNMkNetItkWpIxQwpmJ7DHBTIjPiNZQjxYo";
 const NPSqueryURL =
-  "https://developer.nps.gov/api/v1/parks?api_key=UOZg2ZNMkNetItkWpIxQwpmJ7DHBTIjPiNZQjxYo" +
-  "&stateCode=" +
-  stateCode;
+	"https://developer.nps.gov/api/v1/parks?api_key=UOZg2ZNMkNetItkWpIxQwpmJ7DHBTIjPiNZQjxYo" +
+	"&stateCode=" +
+	stateCode;
 
 $.ajax({
-  url: NPSqueryURL,
-  method: "GET",
+	url: NPSqueryURL,
+	method: "GET",
 }).then(function (response) {
-  console.log(response);
+	console.log(response);
 
-  $("#selectState").val(stateCode);
-  stateCode.push(response.data.contacts.fullName);
-  //----------------------
-  var parkList = response.data.contacts.fullName;
-  // for (var i = 0; i < parkList.length; i++) {
-  //   var opt = parkList[i];
-  //   var el = document.createElement("option");
-  //   el.textContent = opt;
-  //   el.value = opt;
-  //   select.appendChild(el);
-  // }
+	$("#selectState").val(stateCode);
+	stateCode.push(response.data.contacts.fullName);
+	//----------------------
+	var parkList = response.data.contacts.fullName;
+	// for (var i = 0; i < parkList.length; i++) {
+	//   var opt = parkList[i];
+	//   var el = document.createElement("option");
+	//   el.textContent = opt;
+	//   el.value = opt;
+	//   select.appendChild(el);
+	// }
 
-  // need to validate the latitude and longitude
+	// need to validate the latitude and longitude
 
-  // const lat = response.data.latitude;
-  // const lon = response.data.longitude;
-  // call on the weather function and pass these values through
-  console.log(response);
-  //-----verify functionality later:	parkPage(lat, lon);
-  // we need to find a way to stringify the latlon combo in the json selecting thing. They dont have seperate lat and lon
+	// const lat = response.data.latitude;
+	// const lon = response.data.longitude;
+	// call on the weather function and pass these values through
+	console.log(response);
+	//-----verify functionality later:	parkPage(lat, lon);
+	// we need to find a way to stringify the latlon combo in the json selecting thing. They dont have seperate lat and lon
 });
 
 function parkPage(response) {
-  stateCode = stateCode(response.data.fullName);
+	stateCode = stateCode(response.data.fullName);
 
-  //make: response = stateCode(response.xxxxxxxxx)
-  // pull up parks
-  // var city so we can pull up the weather using .dotation
+	//make: response = stateCode(response.xxxxxxxxx)
+	// pull up parks
+	// var city so we can pull up the weather using .dotation
 }
 
 // -------API call to the OpenWeather API---------------This code is fully functional.-----
