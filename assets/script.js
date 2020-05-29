453d116cf9658c7ee3c95995b8b2db26c95c7ec9
Jordan <jordan.a.neill@gmail.com>
2020-05-28 22:37:59 -0500

    added psuedocode

--- assets/script.js
+++ assets/script.js
@@ -2,140 +2,115 @@
 var select = document.getElementById("selectState");
 
 var stateCode = [
-	"AL",
-	"AK",
-	"AS",
-	"AZ",
-	"AR",
-	"CA",
-	"CO",
-	"CT",
-	"DE",
-	"DC",
-	"FM",
-	"FL",
-	"GA",
-	"GU",
-	"HI",
-	"ID",
-	"IL",
-	"IN",
-	"IA",
-	"KS",
-	"KY",
-	"LA",
-	"ME",
-	"MH",
-	"MD",
-	"MA",
-	"MI",
-	"MN",
-	"MS",
-	"MO",
-	"MT",
-	"NE",
-	"NV",
-	"NH",
-	"NJ",
-	"NM",
-	"NY",
-	"NC",
-	"ND",
-	"MP",
-	"OH",
-	"OK",
-	"OR",
-	"PW",
-	"PA",
-	"PR",
-	"RI",
-	"SC",
-	"SD",
-	"TN",
-	"TX",
-	"UT",
-	"VT",
-	"VI",
-	"VA",
-	"WA",
-	"WV",
-	"WI",
-	"WY",
+  "AL",
+  "AK",
+  "AS",
+  "AZ",
+  "AR",
+  "CA",
+  "CO",
+  "CT",
+  "DE",
+  "DC",
+  "FM",
+  "FL",
+  "GA",
+  "GU",
+  "HI",
+  "ID",
+  "IL",
+  "IN",
+  "IA",
+  "KS",
+  "KY",
+  "LA",
+  "ME",
+  "MH",
+  "MD",
+  "MA",
+  "MI",
+  "MN",
+  "MS",
+  "MO",
+  "MT",
+  "NE",
+  "NV",
+  "NH",
+  "NJ",
+  "NM",
+  "NY",
+  "NC",
+  "ND",
+  "MP",
+  "OH",
+  "OK",
+  "OR",
+  "PW",
+  "PA",
+  "PR",
+  "RI",
+  "SC",
+  "SD",
+  "TN",
+  "TX",
+  "UT",
+  "VT",
+  "VI",
+  "VA",
+  "WA",
+  "WV",
+  "WI",
+  "WY",
 ];
 for (var i = 0; i < stateCode.length; i++) {
-	var opt = stateCode[i];
-	var el = document.createElement("option");
-	el.setAttribute("value", opt);
-	el.textContent = opt;
-	select.appendChild(el);
+  var opt = stateCode[i];
+  var el = document.createElement("option");
+  el.setAttribute("value", opt);
+  el.textContent = opt;
+  select.appendChild(el);
 }
 
 $(document).ready(function () {
-	$("#submitBtn").on("click", function () {
-		let stateCode = $("#selectState option:selected").text();
-		console.log(stateCode);
-		populateState(stateCode);
-	});
+  $("#submitBtn").on("click", function () {
+    let stateCode = $("#selectState option:selected").text();
+    console.log(stateCode);
+    populateState(stateCode);
+  });
 });
 $("select").val("option-value");
 
 // Ajax for State
 function populateState(stateCode) {
-	const NPSAPIkey = "UOZg2ZNMkNetItkWpIxQwpmJ7DHBTIjPiNZQjxYo";
-	const NPSqueryURL =
-		"https://developer.nps.gov/api/v1/parks?api_key=UOZg2ZNMkNetItkWpIxQwpmJ7DHBTIjPiNZQjxYo" +
-		"&stateCode=" +
-		stateCode;
-		console.log("good");
+  const NPSAPIkey = "UOZg2ZNMkNetItkWpIxQwpmJ7DHBTIjPiNZQjxYo";
+  const NPSqueryURL =
+    "https://developer.nps.gov/api/v1/parks?api_key=UOZg2ZNMkNetItkWpIxQwpmJ7DHBTIjPiNZQjxYo" +
+    "&stateCode=" +
+    stateCode;
+  console.log("good");
 
-	$.ajax({
-		url: NPSqueryURL,
-		method: "GET",
-	}).then(function (response) {
-		console.log("ajax function running");
-		parkPage(response);
-		$("#selectState").val(stateCode);
-		stateCode.push(response.data.contacts.fullName);
-		//----------------------
-		var parkList = response.data.contacts.fullName;
-		// for (var i = 0; i < parkList.length; i++) {
-		//   var opt = parkList[i];
-		//   var el = document.createElement("option");
-		//   el.textContent = opt;
-		//   el.value = opt;
-		//   select.appendChild(el);
-		// }
-
-		// need to validate the latitude and longitude
-
-		// const lat = response.data.latitude;
-		// const lon = response.data.longitude;
-		// call on the weather function and pass these values through
-		//console.log(response);
-		//-----verify functionality later:	parkPage(lat, lon);
-		// we need to find a way to stringify the latlon combo in the json selecting thing. They dont have seperate lat and lon
-	});
+  $.ajax({
+    url: NPSqueryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log("ajax function running");
+    parkPage(response);
+    $("#selectState").val(stateCode);
+    stateCode.push(response.data.contacts.fullName);
+    //----------------------
+    var parkList = response.data.contacts.fullName;
+    //-----verify functionality later:	parkPage(lat, lon);
+    // we need to find a way to stringify the latlon combo in the json selecting thing. They dont have seperate lat and lon
+  });
 }
 
 function parkPage(parksArray) {
-	console.log(parksArray)
-	// park page will take in an array of many parks and their info and return an array of ONLY the park names
-	//stateCode = stateCode(response.data.fullName);
-	let newNamesArray = parksArray.data.map(function(item){
-		console.log(item.fullName)
-		return "hey"
-	})
-	// for (var i = 0; i < response.length; i++) {
-	// 	var opt = stateCode[i];
-	// 	var el = document.createElement("option");
-	// 	el.setAttribute("value", opt);
-	// 	el.textContent = opt;
-	// 	select.appendChild(el);
-	// }
-
-	//make: response = stateCode(response.xxxxxxxxx)
-	// pull up parks
-	// var city so we can pull up the weather using .dotation
+  console.log(parksArray);
+  // park page will take in an array of many parks and their info and return an array of ONLY the park names
+  //stateCode = stateCode(response.data.fullName);cod
+  let newNamesArray = parksArray.data.map(function (item) {
+    console.log(item.fullName);
+    return "hey";
+  });
 }
 
 // -------API call to the OpenWeather API---------------This code is fully functional.-----
