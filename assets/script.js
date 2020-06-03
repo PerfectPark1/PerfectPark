28256ca7716a071cb542334dc9a3cdd471f6d0b4
CGMcBride <connermcbride@ymail.com>
2020-06-03 17:09:18 -0500

    added map API info

--- assets/script.js
+++ assets/script.js
@@ -135,6 +135,73 @@ function parkPage(parksArray) {
 		// console.log(item.activities[""].names);
 		console.log(lon);
 		console.log(lat);
+		mapboxgl.accessToken = 'pk.eyJ1IjoiY21jYnJpZGUwOSIsImEiOiJja2F6dWpubTAwMjR3MzBwcHE5dmJvaDA1In0.UJbD1XzftoijH31XRb0D3w';
+		var map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [-79.4512, 43.6568],
+			zoom: 13
+		});
+
+		/* given a query in the form "lng, lat" or "lat, lng" returns the matching
+		* geographic coordinate(s) as search results in carmen geojson format,
+		* https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
+		*/
+		var coordinatesGeocoder = function (query) {
+			// match anything which looks like a decimal degrees coordinate pair
+			var matches = query.match(
+				/^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
+			);
+			if (!matches) {
+				return null;
+			}
+
+			function coordinateFeature(lng, lat) {
+				return {
+					center: [lng, lat],
+					geometry: {
+						type: 'Point',
+						coordinates: [lng, lat]
+					},
+					place_name: 'Lat: ' + lat + ' Lng: ' + lng,
+					place_type: ['coordinate'],
+					properties: {},
+					type: 'Feature'
+				};
+			}
+
+			var coord1 = Number(matches[1]);
+			var coord2 = Number(matches[2]);
+			var geocodes = [];
+
+			if (coord1 < -90 || coord1 > 90) {
+				// must be lng, lat
+				geocodes.push(coordinateFeature(coord1, coord2));
+			}
+
+			if (coord2 < -90 || coord2 > 90) {
+				// must be lat, lng
+				geocodes.push(coordinateFeature(coord2, coord1));
+			}
+
+			if (geocodes.length === 0) {
+				// else could be either lng, lat or lat, lng
+				geocodes.push(coordinateFeature(coord1, coord2));
+				geocodes.push(coordinateFeature(coord2, coord1));
+			}
+
+			return geocodes;
+		};
+
+		map.addControl(
+			new MapboxGeocoder({
+				accessToken: mapboxgl.accessToken,
+				localGeocoder: coordinatesGeocoder,
+				zoom: 4,
+				placeholder: 'Try: -40, 170',
+				mapboxgl: mapboxgl
+			})
+		);
 
 		return `
 		<div class="col s12 m6 l4">
@@ -164,6 +231,8 @@ function showParkInfo(e) {
 $(document).ready(function () {
 	$(".slider").slider();
 });
+
+
 
 // -------API call to the OpenWeather API---------------This code is fully functional.-----
 //--------No need to modify the weather section att.--------------------------------
